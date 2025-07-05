2386dcbd0bd3b2140484c2d2d4404ed6f496ca4f
saiganesh74 <ganeshrejeti1@gmail.com>
2025-07-05 08:19:23 +0530

    gitignore updated

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDRqdgXPnn1tiqKOreGOcyJifbEuyy_TdI",
-  authDomain: "naviloop-test.firebaseapp.com",
-  databaseURL: "https://naviloop-test-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "naviloop-test",
-  storageBucket: "naviloop-test.firebasestorage.app",
-  messagingSenderId: "116376630309",
-  appId: "1:116376630309:web:e7f9365947502ace11bbcc"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDRqdgXPnn1tiqKOreGOcyJifbEuyy_TdI",
+    authDomain: "naviloop-test.firebaseapp.com",
+    databaseURL: "https://naviloop-test-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "naviloop-test",
+    storageBucket: "naviloop-test.firebasestorage.app",
+    messagingSenderId: "116376630309",
+    appId: "1:116376630309:web:e7f9365947502ace11bbcc"
+  };
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.database();
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebase.database();
