ccd4fd9073d1cbc7006e41ee57c1394cdf1d33a9
saiganesh74 <ganeshrejeti1@gmail.com>
2025-08-11 09:31:25 +0530

    Theme Toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button"
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
-
+// new comment
   return (
     <Button
       variant="ghost"
