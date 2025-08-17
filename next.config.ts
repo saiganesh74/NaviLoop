e8455092112c6f3a3d913fe8273a638243ab3d91
saiganesh74 <ganeshrejeti1@gmail.com>
2025-08-17 20:05:20 +0530

    Config

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const withPWA = require('next-pwa')({
   skipWaiting: true,
   runtimeCaching: [
     {
-    // eiofjsiof
+    // eiofjsiodsff
       urlPattern: /^https?.*/, 
       handler: 'NetworkFirst',
       options: {
